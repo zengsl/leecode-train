1fd3514345a55c90ae51e3a11f0a7553ea9aa235
zengsl <zengsl0212@foxmail.com>
2024-11-07 18:01:40 +0800

    chore: new train

--- src/main/java/com/leecode/hot100/longestConsecutive/LongestConsecutiveSequence.java
+++ src/main/java/com/leecode/hot100/longestConsecutive/LongestConsecutiveSequence.java
@@ -10,10 +10,32 @@ public class LongestConsecutiveSequence {
             return 0;
         }
 
+        // 将数组中所有元素放入集合
+        Set <Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
 
+        int longestStreak = 0;
+        // 遍历集合中所有的元素，依次判断是否为最长连续子序列的起点，并且计算最长连续子序列的长度。
+        // 最终取“以每个元素为起点的子序列中长度最长”的作为最长子序列，并返回其长度。
+        for (int currentNum : numSet) {
+            // 当前数的前一位数字不在集合中，说明当前数是连续子序列的起点
+            if (!numSet.contains(currentNum - 1)) {
+                int currentStreak = 1;
+                // 循环判断集合中是否存在连续数字
+                while (numSet.contains(currentNum + 1)) {
+                    // 最长子序列长度 + 1
+                    currentStreak += 1;
+                    // 当前数字 + 1 ，为继续判断下一个数字做准备
+                    currentNum += 1;
+                }
+                // 每次循环结束之后，将当前最长子序列长度与最长子序列长度进行比较，取较大的值
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
 
-
-        return 0;
+        return longestStreak;
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/leecode/hot100/moveZero/Solution.java
@@ -0,0 +1,33 @@
+package com.leecode.hot100.moveZero;
+
+import java.util.Arrays;
+
+class Solution {
+    //[0,1,0,3,12]
+    public void moveZeroes(int[] nums) {
+
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+
+        int zeroIndex = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0 && zeroIndex == -1) {
+                zeroIndex = i;
+            } else if(nums[i] != 0){
+                if (zeroIndex != -1) {
+                    nums[zeroIndex] = nums[i];
+                    nums[i] = 0;
+                    zeroIndex += 1;
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+        new Solution().moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
