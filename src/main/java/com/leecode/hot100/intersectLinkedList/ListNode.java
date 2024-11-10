bd53ee0061304d323f35f24e11096f8d9c82df04
zengsl <zengsl0212@foxmail.com>
2024-11-10 22:26:15 +0800

    chore: new train

---
+++ src/main/java/com/leecode/hot100/intersectLinkedList/ListNode.java
@@ -0,0 +1,11 @@
+package com.leecode.hot100.intersectLinkedList;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
