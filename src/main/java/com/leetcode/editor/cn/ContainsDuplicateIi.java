9345609f896bc5a1dc1d4481b7e754cf259e2f61
zengsl <zengsl0212@foxmail.com>
2025-01-12 22:28:45 +0800

    chore: new train

---
+++ src/main/java/com/leetcode/editor/cn/ContainsDuplicateIi.java
@@ -0,0 +1,79 @@
+//Given an integer array nums and an integer k, return true if there are two 
+//distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <
+//= k. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [1,2,3,1], k = 3
+//Output: true
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [1,0,1,1], k = 1
+//Output: true
+// 
+//
+// Example 3: 
+//
+// 
+//Input: nums = [1,2,3,1,2,3], k = 2
+//Output: false
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= nums.length <= 10⁵ 
+// -10⁹ <= nums[i] <= 10⁹ 
+// 0 <= k <= 10⁵ 
+// 
+//
+// Related Topics 数组 哈希表 滑动窗口 👍 751 👎 0
+
+
+package com.leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * [219]Contains Duplicate II
+ */
+public class ContainsDuplicateIi {
+    public static void main(String[] args) {
+        Solution solution = new ContainsDuplicateIi().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        public boolean containsNearbyDuplicate(int[] nums, int k) {
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                Integer prevIndex = map.put(nums[i], i);
+                if(prevIndex != null && i - prevIndex <= k) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /*public boolean containsNearbyDuplicate(int[] nums, int k) {
+            for (int i = 0; i < nums.length; i++) {
+                for (int j = i + 1; j <= i + k && j < nums.length; j++) {
+                    if (nums[i] == nums[j]) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }*/
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
